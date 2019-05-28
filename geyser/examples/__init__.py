7a1a671bc53ec3c89f7c909436c8611dea7f995a
mslaughter <melissaslaughter@me.com>
2019-05-28 16:26:11 -0700

    Move registry updating location.

--- geyser/examples/__init__.py
+++ geyser/examples/__init__.py
@@ -1,2 +1,9 @@
 from .basic_example import BasicJob
 from .handlers import handlers
+
+import geyser.registry
+
+
+geyser.registry.JOB_MODULES = geyser.registry.JOB_MODULES + [
+    "geyser.examples"
+]
--- geyser/examples/basic_example/__init__.py
+++ geyser/examples/basic_example/__init__.py
@@ -1,9 +1,2 @@
 from .basic_job import BasicJob, make_basic_job
 from .basic_handler import BasicHandler
-
-from geyser import JOB_MODULES
-
-
-JOB_MODULES = JOB_MODULES + [
-    "geyser.examples"
-]
