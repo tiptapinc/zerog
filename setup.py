e405d537e55637accd1db6f26851dd1c56ac6a6f
Frank Slaughter <fgs@tower3.com>
2020-01-29 14:24:39 -0800

    add dependencies to setup.py, update .gitignore

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version=open('VERSION').read().strip(),
     author='MotiveMetrics',
     install_requires=[
-        'tornado>=4.5', 'marshmallow>=3.0.5'
+        'beanstalkc3>=0.4.0',
+        'couchbase>=2.5.10',
+        'marshmallow>=3.0.5',
+        'tornado>=4.5'
     ],
     packages=find_packages(exclude=["tests"])
-    # package_dir={"zerog": "zerog"}
 )
