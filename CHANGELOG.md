e13c164db6aa6a642478a8d86a382c453104aa08
Peng Gao <puerile22@gmail.com>
2022-05-10 14:28:13 -0700

    Sv2 2822 (#24)
    * Fix import for new couchbase version
    
    * Update couchbase version in requirements
    
    * Lock couchbase version to 3.2.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+##v0.0.40
+* lock couchbase version to 3.2.7
+
 ##v0.0.39
 * add uuid handler for dumping jobs
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='MotiveMetrics',
     install_requires=[
         'beanstalkc3>=0.4.0',
-        'couchbase>=3.0',
+        'couchbase==3.2.7',
         'marshmallow>=3.0.5',
         'psutil',
         'tornado>=4.5'
