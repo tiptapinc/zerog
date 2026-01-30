87ed0e3223b861dbb27cf344975c78bdf96dbc90
puerile22 <puerile22@gmail.com>
2026-01-30 13:38:32 -0800

    Update

--- zerog/datastores/couchbase_datastore.py
+++ zerog/datastores/couchbase_datastore.py
@@ -4,10 +4,11 @@
 Copyright (c) 2020 MotiveMetrics. All rights reserved.
 
 """
-from couchbase.cluster import Cluster, ClusterOptions, PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.auth import PasswordAuthenticator
 from couchbase.management.buckets import BucketManager
+from couchbase.options import ClusterOptions
 import couchbase.exceptions
-import datetime
 import psutil
 
 import logging
